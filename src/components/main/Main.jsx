51c8b8ef47c6f897a6e9f953b81b46af710488fe
Veera Jayanth K <veerajayanth07@gmail.com>
2024-10-28 10:22:08 +0530

    chnaged the OCT_Resume

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import work from './../../assets/images/work.jpg'
-import pdf from './../../assets/images/Veera Jayanth K Resume.pdf'
+import pdf from './../../assets/images/KVJ - OCT Resume.pdf'
 import './../../App.css'
 
 function Main(){
@@ -10,8 +10,8 @@ function Main(){
                 <h1 id = 'hi'>Hi, I am <br/></h1>
                     <h1>Veera Jayanth Kuppam </h1>
                     <p>I'm an Engineering Graduate, and enthusiatic of learning the new skills and using them to solve the Industry Relevant Problems.</p>
-                    <a href={pdf} view="Veera Jayanth K Resume.pdf"><button className="button">View resume</button></a>
-                    <a href={pdf} download="Veera Jayanth K Resume.pdf"><button className="button">Download resume</button></a>
+                    <a href={pdf} view="KVJ - OCT Resume.pdf"><button className="button">View resume</button></a>
+                    <a href={pdf} download="KVJ - OCT Resume.pdf"><button className="button">Download resume</button></a>
                 </div>
                 <img src={work} />
         </section>  
